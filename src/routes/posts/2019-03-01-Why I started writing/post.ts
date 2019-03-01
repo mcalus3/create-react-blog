1b5215ca02ceacca175ed213f0b903600cf15a60
mcalus3 <marek-calus@wp.pl>
2019-03-01 13:43:57 +0100

    Write draft of first post and change font size on posts.

--- src/routes/posts/2019-03-01-Why I started writing/post.ts
+++ src/routes/posts/2019-03-01-Why I started writing/post.ts
@@ -1,6 +1,6 @@
 export default {
   title: `Why I started writing?`,
-  tags: ["blogging", "learning"],
+  tags: ["blog", "thoughts"],
   spoiler: "Announcment of the blog.",
   getContent: () => import("./document.mdx")
 };
